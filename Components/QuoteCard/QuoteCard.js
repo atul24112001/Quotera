cf4e9f514d3c82810be9ff81109cd3e87b9a075e
Atul <atulmorchhlay204@gmail.com>
2022-02-06 15:56:20 +0530

    Complete

--- Components/QuoteCard/QuoteCard.js
+++ Components/QuoteCard/QuoteCard.js
@@ -1,21 +1,53 @@
-import React from 'react';
-import { FaTwitter, FaTumblr } from "react-icons/fa";
+import React, { useEffect, useState } from 'react';
+import { FaTwitter, FaTumblr, FaInfoCircle } from "react-icons/fa";
 
-export default function QuoteCard({ bgColor }) {
+export default function QuoteCard({ setBgColor, bgColor }) {
+    const [quotes, SetQuotes] = useState(null);
+    const [quoteIndex, setQuoteIndex] = useState(0);
 
+    useEffect(() => {
+        const genrateQuote = () => {
+            fetch("https://travel-junky-default-rtdb.firebaseio.com/quotes.json")
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    SetQuotes(data);
+                });
+
+        }
+
+        genrateQuote();
+    }, []);
+
+    const newQuoteHandler = () => {
+        setQuoteIndex(prev => prev + 1);
+
+        const col = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"];
+
+        let element = "";
+        for (let index = 1; index < 7; index++) {
+            element = element + col[Math.floor(Math.random() * 16)];
+        }
+
+        if (element.length === 6) {
+            const mainColor = `#${element}`;
+            setBgColor(mainColor)
+        }
+    }
     return (
-        <div className='bg-white w-[50vw] h-[50vh] rounded text-4xl px-[3vw]'>
-            <div className='py-[5vh]'>
-                <p className={`bg-clip-text text-transparent ${bgColor}`}><span className='text-6xl'>“</span>If you cannot do great things, do small things in a great way.<span className='text-6xl'>”</span></p>
-                <p className={`bg-clip-text text-right text-transparent ${bgColor}`}>- Napoleon Hill</p>
+        <div className='bg-white w-[90vw] md:w-[70vw] lg:w-[50vw] h-[50vh] relative rounded text-2xl md:text-3xl lg:text-4xl px-[3vw]'>
+            <div className='py-[5vh] font-bold'>
+                <p style={{ backgroundColor: `${bgColor}` }} className={`bg-clip-text text-transparent text-center`}><span className='text-6xl'>“</span>{quotes ? quotes[quoteIndex].text : "Genius is one percent inspiration and ninety-nine percent perspiration."}<span className='text-6xl'>”</span></p>
+                <p style={{ backgroundColor: `${bgColor}` }} className={`bg-clip-text absolute bottom-[18vh] right-[3vw] text-right text-transparent`}>- {quotes ? quotes[quoteIndex].author : "Thomas Edison"}</p>
 
             </div>
-            <div className='flex justify-between items-center text-3xl text-white py-[2vh]'>
+            <div className='flex justify-between bo items-center text-1xl absolute bottom-[5vh] w-[84vw] md:w-[64vw] lg:w-[44vw] text-white py-[2vh]'>
                 <div className='flex items-center'>
-                    <button className={`${bgColor} p-[1vh] rounded m-[1vh]`}><FaTwitter /></button>
-                    <button className={`${bgColor} p-[1vh] rounded m-[1vh]`}><FaTumblr /></button>
+                    <a href='#' style={{ backgroundColor: `${bgColor}` }} className={` p-[1vh] rounded m-[1vh]`}><FaTwitter /></a>
+                    <a href='#' style={{ backgroundColor: `${bgColor}` }} className={` p-[1vh] rounded m-[1vh]`}><FaTumblr /></a>
                 </div>
-                <button className={`${bgColor} rounded py-[1vh] px-[1vw]`}>New Quote</button>
+                <button style={{ backgroundColor: `${bgColor}` }} onClick={newQuoteHandler} className={` rounded py-[1vh] px-[1vw]`}>New Quote</button>
             </div>
         </div>
     );
